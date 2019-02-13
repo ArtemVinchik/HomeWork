30755d42177640471b8a4dcf01aa1b11fe978d2d
ArtemVinchik <46972860+ArtemVinchik@users.noreply.github.com>
2019-02-13 18:52:11 +0200

    Add files via upload

---
+++ FindMatrixData.java
@@ -0,0 +1,11 @@
+import GrLibrary.GrLibrary;
+
+import java.awt.*;
+
+public class FindMatrixData {
+
+    public static void main(String[] args) {
+        new GrLibrary();
+    }
+}
+//public class FindMatrixData { }
